9efcd60adf0ded7e840905d25fc007a3c3a57fdb
“Saventiiy” <breadduckwithpigpeppa@gmail.com>
2024-06-08 11:10:42 +0200

    Added notification when word is changing in worker

--- app/src/main/java/com/saventiy/wordly/MainActivity.kt
+++ app/src/main/java/com/saventiy/wordly/MainActivity.kt
@@ -1,20 +1,17 @@
 package com.saventiy.wordly
 
-import android.app.PendingIntent
-import android.content.Intent
-import android.content.pm.PackageInstaller
-import android.net.Uri
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.saventiy.wordly.screens.main.MainScreen
 import com.saventiy.wordly.ui.theme.WordlyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,7 +20,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             WordlyTheme {
                 // A surface container using the 'background' color from the theme
@@ -34,6 +30,17 @@ class MainActivity : ComponentActivity() {
                     MainNavigation()
                 }
             }
+        }
+
+        checkNotificationPermission()
+    }
+
+
+    //TODO refactor it
+    private fun checkNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            this.registerForActivityResult(ActivityResultContracts.RequestPermission()) {}
+                .launch(Manifest.permission.POST_NOTIFICATIONS)
         }
     }
 }
--- app/src/main/java/com/saventiy/wordly/screens/worker/ChangeWordWorker.kt
+++ app/src/main/java/com/saventiy/wordly/screens/worker/ChangeWordWorker.kt
@@ -1,9 +1,16 @@
 package com.saventiy.wordly.screens.worker
 
+import android.annotation.SuppressLint
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.saventiy.wordly.MainActivity
+import com.saventiy.wordly.R
 import com.saventiy.wordly.domain.usecase.local.GetAllCollectionsUseCase
 import com.saventiy.wordly.screens.widget.WordWidgetReceiver.Companion.updateWidget
 import dagger.assisted.Assisted
@@ -25,10 +32,32 @@ internal class ChangeWordWorker @AssistedInject constructor(
                 .onEach { collections ->
                     val word = collections.find { it.isActive }?.collection?.random()
                     updateWidget(word.toString(), context)
+                    sendNotification(word = word)
                 }
                 .collect()
             Result.success()
         } catch (e: Exception) {
             Result.retry()
         }
+
+
+    //TODO should I moved it?
+    @SuppressLint("MissingPermission")
+    private fun sendNotification(word: String?) {
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent =
+            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val builder = NotificationCompat.Builder(context, "wordly")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("wordly")
+            .setContentText(word.toString())
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+        val notificationManager = NotificationManagerCompat.from(context)
+        notificationManager.notify(1, builder.build())
+    }
 }
